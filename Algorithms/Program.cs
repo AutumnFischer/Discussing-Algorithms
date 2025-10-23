575ffeebe4a3cc888d1fc364ce9eae9b6a758312
Vicky <vickyn5104@gmail.com>
2025-10-23 15:16:37 -0400

    made tweaks
    removed unnecessary code blocks, stored array length in variable

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,58 +4,52 @@
     {
         static void Main(string[] args)
         {
-            int[] numbers = {-33, 43, 355, 308, -150, -184, 109, 216, 72, 45, 255, 386, 288, -2, -206, 325, 204, -2, 352, -252, 469, -88, 145, -214, 203, -102, -250, 368, 25, 398, -223, -222, 363, -81};
+            int[] numbers = [-33, 43, 355, 308, -150, -184, 109, 216, 72, 45, 255, 386, 288, -2, -206, 325, 204, -2, 352, -252, 469, -88, 145, -214, 203, -102, -250, 368, 25, 398, -223, -222, 363, -81];
+            int length = numbers.Length;
 
             //Mean
             double sum = 0;
             foreach (int num in numbers) { 
                 sum += num;
             }
-            double mean = Math.Round(sum / numbers.Length);
+            double mean = Math.Round(sum / length);
             Console.WriteLine($"The mean of this dataset is: {mean}!");
 
             //Median
             Array.Sort(numbers);
             double median = 0;
             //if even
-            if (numbers.Length % 2 == 0)
+            if (length % 2 == 0)
             {
-                int mid1 = (numbers.Length / 2) - 1;
-                int mid2 = numbers.Length / 2;
+                int mid1 = (length / 2) - 1;
+                int mid2 = length / 2;
                 median = (double)(numbers[mid1] + numbers[mid2]) / 2;
             }
             //if odd
             else {
-                int mid = numbers.Length / 2;
+                int mid = length / 2;
                 median = numbers[mid];
             }
             Console.WriteLine($"The median of this dataset is: {median}!");
 
             //Min
             int min = numbers[0];
-            foreach (int num in numbers)
+            for (int i = 1; i < length; i++)
             {
-                if (num < min)
+                if (numbers[i] < min)
                 {
                     min = num;
-                }
-                else {
-                    //do nothing
                 }
             }
             Console.WriteLine($"The minimum value of this dataset is: {min}!");
 
             //Max
             int max = numbers[0];
-            foreach (int num in numbers)
+            for (int i = 1; i < length; i++)
             {
-                if (num > max)
+                if (numbers[i] > max)
                 {
                     max = num;
-                }
-                else
-                {
-                    //do nothing
                 }
             }
             Console.WriteLine($"The maximum value of this dataset is: {max}!");
